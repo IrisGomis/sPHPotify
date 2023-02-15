6886cbd4be27cf3b471145ec78abfe0d53d40b08
IrisGomis <gomis.f.iris@gmai.com>
2023-02-15 15:44:58 +0100

    Formulario hecho, falta maquetar

--- FRONT/sphpotify-react/src/components/moleculas/SongsForm.jsx
+++ FRONT/sphpotify-react/src/components/moleculas/SongsForm.jsx
@@ -1,53 +1,54 @@
 import React from "react";
-import '../moleculas/styles/songsForm.css'
-import LogoBM from "../../images/logo/para-fondo-negro.png"
+import '../moleculas/styles/songsForm.css';
+import LogoBM from "../../images/logo/para-fondo-negro.png";
 
 function Search() {
   return (
     <>
-    <img className="logoBM" src={LogoBM} alt="" />
-    <form action="" method="">
-
-      <div className="img-hole">
-        <label for="user">Img</label>
-        <input type="img" id="img-name" placeholder="Insert New img."></input>
+    <div className="container">
+      <img className="logoBM" src={LogoBM} alt="" />
+      <div className="img-added">
+        <img src="" alt=""/>
       </div>
-      <form action="upload.php" method="post" enctype="multipart/form-data">
-        <input type="file" name="image">
-        <input type="submit" value="Upload Image">
-      </form>
-      <div className="form-elements">
+      <div className="img-imput">
+        <input type="file" name="image"  />
+        <br></br>
+        <input type="submit" value="Upload Image" />
+      </div>
+      <form className="form-items" action="" method="">
         <div className="elements-group">
           <label for="track">Song</label>
-          <input type="text" id="track-name" placeholder="Insert New Song"></input>
+            <input type="text" id="track-name" placeholder="Insert New Song"></input>
         </div>
 
         <div className="elements-group">
           <label for="url">URL</label>
-          <input type="url" id="url-name" placeholder="URL"></input>
+            <input type="url" id="url-name" placeholder="URL"></input>
         </div>
 
         <div className="elements-group">
           <label for="artist">Artist</label>
-          <input type="text" id="artist-name" placeholder="Insert New Artist"></input>
+            <input type="text" id="artist-name" placeholder="Insert New Artist"></input>
         </div>
 
         <div className="elements-group">
           <label for="genre">Genre</label>
-          <input type="text" id="genre-name" placeholder="Insert Genre"></input>
+            <input type="text" id="genre-name" placeholder="Insert Genre"></input>
         </div>
 
         <div className="elements-group">
           <label for="date">Date</label>
-          <input type="date" id="date-name" placeholder="Insert date."></input>
+            <input type="date" id="date-name" placeholder="Insert date."></input>
         </div>
 
         <div className="elements-group">
           <label for="user">User</label>
-          <input type="text" id="user-name" placeholder="Insert New User."></input>
+            <input type="text" id="user-name" placeholder="Insert New User."></input>
         </div>
-      </div>
-    </form>
+        <button>Add Song</button>
+      </form>
+
+    </div>
     </>
   );
 }
---
+++ FRONT/sphpotify-react/src/components/moleculas/laDePueba.js
@@ -0,0 +1,43 @@
+<div className="dad">
+      <img className="logoBM" src={LogoBM} alt="" />
+      <div className="image-thumbnail">
+        <img src="" alt=""/>
+      </div>
+      <div>
+        <input type="file" name="image" />
+        <input type="submit" value="Upload Image" />
+      </div>
+      <form className="form-items" action="" method="">
+          <div className="form-elements">
+            <div className="elements-group">
+              <label className="label-name" for="track">Song</label>
+              <input className="imput-name" type="text" id="track-name" placeholder="Insert New Song"></input>
+            </div>
+
+            <div className="elements-group">
+              <label className="label-name" for="url">URL</label>
+              <input className="imput-name" type="url" id="url-name" placeholder="URL"></input>
+            </div>
+
+            <div className="elements-group">
+              <label className="label-name" for="artist">Artist</label>
+              <input className="imput-name" type="text" id="artist-name" placeholder="Insert New Artist"></input>
+            </div>
+
+            <div className="elements-group">
+              <label className="label-name" for="genre">Genre</label>
+              <input className="imput-name" type="text" id="genre-name" placeholder="Insert Genre"></input>
+            </div>
+
+            <div className="elements-group">
+              <label className="label-name" for="date">Date</label>
+              <input className="imput-name" type="date" id="date-name" placeholder="Insert date."></input>
+            </div>
+
+            <div className="elements-group">
+              <label className="label-name" for="user">User</label>
+              <input className="imput-name" type="text" id="user-name" placeholder="Insert New User."></input>
+            </div>
+          </div>
+      </form>
+    </div>
